b0f53b303dcced38cf972e8bfee6865a16aa7aea
venkat <vikkii89@gmail.com>
2016-02-07 14:41:09 -0800

    small app related to IOS swift tableviews

--- tableViews/ViewController.swift
+++ tableViews/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate{
+    
+    var cellContent = ["kotu","joga","sibyala","dwaram"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4 // if you want to have 3 sections in your table view
+    }
+    
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cellContent[indexPath.row]
+        return cell
+    
+    }
 
 }
 
