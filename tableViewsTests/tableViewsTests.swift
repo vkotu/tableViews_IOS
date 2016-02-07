b48a93e552c9454b7990b8d0350621028d61bcb5
venkat <vikkii89@gmail.com>
2016-02-07 00:49:16 -0800

    Initial Commit

---
+++ tableViewsTests/tableViewsTests.swift
@@ -0,0 +1,36 @@
+//
+//  tableViewsTests.swift
+//  tableViewsTests
+//
+//  Created by Venkat Kotu on 2/7/16.
+//  Copyright © 2016 VenkatKotu. All rights reserved.
+//
+
+import XCTest
+@testable import tableViews
+
+class tableViewsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
